74bfa3b9a7aea8e899924185f1153c43ed22c85a
l105021115 <105021115@live.asia.edu.tw>
2017-02-24 14:56:38 +0800

    hw submit

---
+++ src/hw00.java
@@ -0,0 +1,15 @@
+/*
+* Topic: 第一次作業繳交
+* Date: 2017/02/24
+* Author: 105021115 陳邦天
+*/
+public class hw00 {
+
+	public static void main(String[] args) {
+		System.out.println("系級:資工系(資電組)1A");
+		System.out.println("學號:105021115");
+		System.out.println("姓名:陳邦天");
+
+	}
+
+}
